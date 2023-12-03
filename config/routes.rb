41ac2f9861c2da43037ce090e7be3a556e52fbd3
Kinoken0413 <kengo.kinouchi@gmail.com>
2023-12-03 10:52:11 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
   devise_for :users
 end
